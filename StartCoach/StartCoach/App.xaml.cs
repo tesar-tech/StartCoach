a941786be5a63069f7e5329e28fbf70a6eb4dce2
tesar-tech <33880579+tesar-tech@users.noreply.github.com>
2020-04-04 20:37:25 +0200

    Init commit

---
+++ StartCoach/StartCoach/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using StartCoach.Services;
+using StartCoach.Views;
+
+namespace StartCoach
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}

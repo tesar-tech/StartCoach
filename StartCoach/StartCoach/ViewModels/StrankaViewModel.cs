11fc64be0f5ec099e49c3db6a82823a5d1d51f9c
souckmi2 <souckmi2@fbmi.cvut.cz>
2020-04-20 17:21:52 +0200

    Stranka - binding in progress

---
+++ StartCoach/StartCoach/ViewModels/StrankaViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace StartCoach.ViewModels
+{
+    public class StrankaViewModel : BaseViewModel
+    {
+        public StrankaViewModel()
+        {
+            testCommand = new Command(testMethod);
+        }
+
+        public ICommand testCommand { get; }
+        private string test = "test";
+
+        public string Test { get => test; set => SetProperty(ref test, value); }
+
+        public void testMethod()
+        {
+            Test = DateTime.Now.ToString();
+        }
+        
+    }
+}
